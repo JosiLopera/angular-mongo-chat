e694ed837d62148cd8c7bd01a89bb968596445f6
JosiLopera <joxi.lopera@gmail.com>
2015-04-08 14:40:09 +0200

    Added Factory group, Created view group  and Modified seed.js

--- server/api/group/group.model.js
+++ server/api/group/group.model.js
@@ -3,10 +3,13 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var User = require('../user/user.model');
+
 var GroupSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+    _creator : { type: Schema.Types.ObjectId, ref: 'User' },
+    name: String,
+    info: String,
+    active: Boolean
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
